fe937b7960e1161759fbc39a20f3714ddcc6d825
TylerAUhl <tyleruhl@ufl.edu>
2019-02-05 12:54:35 -0500

    Started work on turns

--- Tank-Wars-Unity/Assets/Scripts/ObjectClicker.cs
+++ Tank-Wars-Unity/Assets/Scripts/ObjectClicker.cs
@@ -6,7 +6,35 @@ public class ObjectClicker : MonoBehaviour {
 
     public GameObject tankClicked = null;
     public GameObject tileClicked = null;
+    public GameObject redTank;
+    public GameObject blueTank;
     public float yVal = 1f;
+    public int[,] terrainMapOne;
+    public Grid grid;
+    public int playerTurn;
+
+    private void Start(){
+      terrainMapOne = new int[,]
+        {
+            {2,1,2,4,1,1,2,3,2,4},
+            {3,2,3,2,1,1,0,2,0,2},
+            {2,0,2,4,1,1,2,4,2,4},
+            {4,2,3,2,1,1,3,2,3,2},
+            {2,0,2,0,1,1,2,0,2,4},
+            {3,2,4,2,1,1,0,2,0,2},
+            {2,4,2,0,1,1,2,4,2,3},
+            {3,2,3,2,1,1,3,2,3,2},
+            {2,4,2,4,1,1,2,4,2,4},
+            {0,2,0,2,1,1,0,2,0,2}
+        };
+
+      redTank = GameObject.Find("redTank");
+      blueTank = GameObject.Find("blueTank");
+
+      grid = new Grid(terrainMapOne, 4, 0, 5, 9);
+
+      playerTurn = 1;
+    }
 
     private void Update()
     {
@@ -25,15 +53,20 @@ public class ObjectClicker : MonoBehaviour {
                         if(hit.transform.gameObject.tag == "Tank"){
                         	tankClicked = hit.transform.gameObject;
                         }
-                       	else if(hit.transform.gameObject.tag == "Tile" && tankClicked != null){
+                       	else if(hit.transform.gameObject.tag == "Tile" && tankClicked != null)
+                        {
                        		tileClicked = hit.transform.gameObject;
-              				//print("tankClicked: ");
-              				//print(tankClicked);
-              				//print("tileClicked: ");
-              				//print(tileClicked);
-              				tankClicked.transform.position = new Vector3(tileClicked.transform.position.x, yVal,tileClicked.transform.position.z);
-              				tileClicked = null;
-              				tankClicked = null;
+                            if(grid.canMove(playerTurn, (int)tankClicked.transform.position.x, (int)tankClicked.transform.position.z))
+                            {
+              				        tankClicked.transform.position = new Vector3(tileClicked.transform.position.x, yVal,tileClicked.transform.position.z);
+              				        tileClicked = null;
+              				        tankClicked = null;
+                              if(++playerTurn > 2)
+                              {
+                                playerTurn = 1;
+                              }
+                              print(playerTurn);
+                            }
                        	}
                     }
                 }
