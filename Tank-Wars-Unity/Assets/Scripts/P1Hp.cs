f05b7aa48c3d5aaead87f6676a9e82f8b29db469
Daniel <danielfrystacky@gmail.com>
2019-02-08 16:43:36 -0500

    Added player Hp display

---
+++ Tank-Wars-Unity/Assets/Scripts/P1Hp.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class P1Hp : MonoBehaviour
+{
+    Text p1HpDisplay;
+    ObjectClicker Player1Hp;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        Player1Hp = FindObjectOfType<ObjectClicker>();
+        p1HpDisplay = GetComponent<Text>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Debug.Log("Players 1 hp is: " + Player1Hp.getObjectClickerGrid().getPlayerHealth(1));
+        p1HpDisplay.text = Player1Hp.getObjectClickerGrid().getPlayerHealth(1).ToString();
+    }
+}
---
+++ Tank-Wars-Unity/Assets/Scripts/PlayerGUI.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerGUI : MonoBehaviour
+{
+    //Text p1HpDisplay;
+    //Grid Player1Hp;
+    //int p1hp;
+
+    void Start()
+    {
+       // Player1Hp = new Grid();
+        //p1HpDisplay = GetComponent<Text>();
+        //p1hp = Player1Hp.getPlayerHealth(1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+       // Debug.Log("Player 1 hp : " + p1hp);
+        //p1HpDisplay.text = p1hp.ToString();
+    }
+}
