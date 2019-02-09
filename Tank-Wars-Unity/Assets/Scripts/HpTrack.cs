13ab6830369e79fa5e4ec6343a7d59c7ccc07e35
Daniel Frystacky <danielfrystacky@gmail.com>
2019-02-08 23:55:14 -0500

    Functional working end game screen

---
+++ Tank-Wars-Unity/Assets/Scripts/HpTrack.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class HpTrack : MonoBehaviour
+{
+    [SerializeField] int players1Hp = 1;
+    [SerializeField] int players2Hp = 1;
+
+    int currentSceneIndex;
+
+    ObjectClicker playerHp;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        playerHp = FindObjectOfType<ObjectClicker>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (currentSceneIndex == 1)
+        {
+            players1Hp = playerHp.getObjectClickerGrid().getPlayerHealth(1);
+            players2Hp = playerHp.getObjectClickerGrid().getPlayerHealth(2);
+        }
+    }
+
+    public int GetPlayersHp(int x)
+    {
+        if(x == 1)
+        {
+            return players1Hp;
+        }
+        else
+        {
+            return players2Hp;
+        }
+            
+    }
+}
---
+++ Tank-Wars-Unity/Assets/Scripts/VistoryScreen.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VistoryScreen : MonoBehaviour
+{
+    [SerializeField] GameObject player1Wins;
+    [SerializeField] GameObject player2Wins;
+
+    HpTrack checkPlayerHp;
+
+    int player1Hp;
+    int player2Hp;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        //checkPlayerHp = FindObjectOfType<HpTrack>();
+
+        //player1Hp = checkPlayerHp.GetPlayersHp(1);
+        //player2Hp = checkPlayerHp.GetPlayersHp(2);
+        
+
+    }
+
+     //Update is called once per frame
+    void Update()
+    {
+        checkPlayerHp = FindObjectOfType<HpTrack>();
+        player1Hp = checkPlayerHp.GetPlayersHp(1);
+        player2Hp = checkPlayerHp.GetPlayersHp(2);
+        Debug.Log("players 1 hp is " + player1Hp);
+        Debug.Log("players 2 hp is " + player2Hp);
+
+        if (player1Hp == 0)
+        {
+            player2Wins.SetActive(true);
+        }
+        else if(player2Hp == 0)
+        {
+            player1Wins.SetActive(true);
+        }
+        else
+        {
+            Debug.Log("No one won :(");
+        }
+    }
+}
