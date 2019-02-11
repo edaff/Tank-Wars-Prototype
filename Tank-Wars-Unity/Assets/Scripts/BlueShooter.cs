9034b84548db07d3fc97edcf96b7da98d492dd27
Daniel <danielfrystacky@gmail.com>
2019-02-11 14:38:58 -0500

    changed shooting for red and blue tank

---
+++ Tank-Wars-Unity/Assets/Scripts/BlueShooter.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlueShooter : MonoBehaviour
+{
+    [SerializeField] GameObject projectile;
+    [SerializeField] GameObject gun;
+
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            Fire();
+        }
+    }
+
+
+    public void Fire()
+    {
+        Instantiate(projectile, gun.transform.position, Quaternion.identity);
+    }
+}
---
+++ Tank-Wars-Unity/Assets/Scripts/RedShooter.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RedShooter : MonoBehaviour
+{
+    [SerializeField] GameObject projectile;
+    [SerializeField] GameObject gun;
+
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            Fire();
+        }
+    }
+
+
+    public void Fire()
+    {
+        Instantiate(projectile, gun.transform.position, Quaternion.identity);
+    }
+}
